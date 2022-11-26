c81018593991f27a83a06fae7c2fc68cd3a8491f
Austin <79065594+austintatiousness@users.noreply.github.com>
2022-11-25 19:07:23 -0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -9,15 +9,15 @@ let package = Package(
         ],
     dependencies: [
         .package(
-            url: "https://github.com/PureSwift/Silica.git",
+            url: "https://github.com/austintatiousness/Silica.git",
             .branch("master")
         ),
         .package(
-            url: "https://github.com/PureSwift/Cairo.git",
+            url: "https://github.com/austintatiousness/Cairo.git",
             .branch("master")
         ),
         .package(
-            url: "https://github.com/PureSwift/SDL.git",
+            url: "https://github.com/austintatiousness/SDL.git",
             .branch("master")
         )
     ],
