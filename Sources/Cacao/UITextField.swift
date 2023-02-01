c3577ac3631fd9267ce9af5c2c417f5fb78e3265
Austin <79065594+austintatiousness@users.noreply.github.com>
2023-02-01 01:31:32 -0600

    Update UITextField.swift
    Started work on insertion points. Next will need to have insertion point actually draw. Started fixing bug where too many characters (especially hidden ones) were being added to the TextField that didn't need to be.

--- Sources/Cacao/UITextField.swift
+++ Sources/Cacao/UITextField.swift
@@ -1,5 +1,3 @@
-///Written by Austin Clow
-
 import struct Foundation.CGFloat
 import struct Foundation.CGPoint
 import struct Foundation.CGSize
@@ -7,7 +5,7 @@ import struct Foundation.CGRect
 import struct Foundation.TimeInterval
 import Silica
 
-/// A control that allows for Text input.
+/// A control that offers a binary choice, such as On/Off.
 ///
 /// The `UISwitch` class declares a property and a method to control its on/off state.
 open class UITextField: UIControl {
@@ -33,40 +31,43 @@ open class UITextField: UIControl {
     }
 
 	open override func pressesBegan(_ presses: Set<UIPress>, with event: UIPressesEvent?) {
-       	 print("presses began with")
-       	 if let event = event {
-       	 	self.text = self.text! + event.characters
-       	 	print(self.text!)
+       	 if let event = event, let first = event.characters.first, var text = self.text {
+       	 	if self.insertionPoint_ == text.count {
+       	 		self.text = text + "\(first)"
+       	 		insertionPoint_ = self.text!.count
+       	 	} else {
+       	 		text.insert(first, at: text.index(text.startIndex, offsetBy: self.insertionPoint_))
+				self.insertionPoint_ += 1
+       	 		self.text = text
+       	 	}
+       	 	
        	 }
     	}
     	
     	open override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-    		print("becoming first responder.")
     		self.becomeFirstResponder()
     		self.focused = true
+    		self.insertionPoint_ = 0
     		
     	}
     	
+	private var insertionPoint_: Int = 0
+	private var insertionLength_: Int = 0
 
-
-	open var text: String? = "1" { didSet { setNeedsDisplay() } }
+	open var text: String? = "" { didSet { setNeedsDisplay() } }
 	
-	open var font: UIFont = UIFont(name: "Helvetica", size: 17)! { didSet { setNeedsDisplay() } }
+	open var font: UIFont = UIFont(name: "Helvetica", size: 28)! { didSet { setNeedsDisplay() } }
     
     	open var textColor: UIColor = .black { didSet { setNeedsDisplay() } }
     
     	open var textAlignment: TextAlignment = .left { didSet { setNeedsDisplay() } }
 	
 	open override func draw(_ rect: CGRect) {
-        
-        guard let context = UIGraphicsGetCurrentContext()
-            else { return }
-        
-        var attributes = TextAttributes()
-        attributes.font = font
-        attributes.color = textColor
-        attributes.paragraphStyle.alignment = textAlignment
-        
-        text?.draw(in: self.bounds, context: context, attributes: attributes)
+        	guard let context = UIGraphicsGetCurrentContext() else { return }
+        	var attributes = TextAttributes()
+        	attributes.font = font
+        	attributes.color = textColor
+        	attributes.paragraphStyle.alignment = textAlignment
+        	text?.draw(in: self.bounds, context: context, attributes: attributes)
     }
 }
